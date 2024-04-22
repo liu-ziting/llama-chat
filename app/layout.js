3c58f58a74c62f24763ba1c1fb48b037f3ec0959
liuziting <57311725+liu-ziting@users.noreply.github.com>
2024-04-22 14:55:21 +0800

    add prompt tip

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,13 @@ export default function RootLayout({ children }) {
                     rel="icon"
                     href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
                 />
+                <script type="text/javascript">
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "m0go9466r4");
+                </script>
             </head>
             <body>
                 {children}
