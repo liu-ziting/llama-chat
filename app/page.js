a318ff5c7b997fe384f92619e089fda9ac0362bb
liuziting <57311725+liu-ziting@users.noreply.github.com>
2024-04-22 10:43:08 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -227,22 +227,11 @@ export default function HomePage() {
         <>
             {/* <CallToAction /> */}
             <nav className="sm:pt-8 pt-4 px-4 sm:px-12 flex items-center">
-                <div className="text-xl pr-3 font-semibold text-gray-500">Chat with</div>
                 <div className="font-semibold text-gray-500 sm:text-center">
                     <Dropdown models={MODELS} selectedModel={model} setModel={setModel} />
                 </div>
                 <div className="flex-grow"></div>
                 <div className="justify-end">
-                    <a
-                        className="inline-flex items-center px-3 py-2 mr-3 text-sm font-semibold text-gray-700 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                        href="https://github.com/replicate/chat"
-                    >
-                        <CodeBracketIcon
-                            className="w-5 h-5 text-gray-500 sm:mr-2 group-hover:text-gray-900"
-                            aria-hidden="true"
-                        />{' '}
-                        <span className="hidden sm:inline">Clone on GitHub</span>
-                    </a>
                     <button
                         type="button"
                         className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
